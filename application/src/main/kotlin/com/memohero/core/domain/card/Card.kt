7b0c0bd96363caed98eb80679974a5927161c4ef
Victor_Grycuk <victor@grycuk.net>
2022-12-29 22:56:51 -0300

    Add classes for spaced repetition

--- application/src/main/kotlin/com/memohero/core/domain/card/Card.kt
+++ application/src/main/kotlin/com/memohero/core/domain/card/Card.kt
@@ -4,5 +4,5 @@ data class Card (
     val userId: String,
     val front: String,
     val back: String,
-    val repetition: Int = 0 // The count of consecutive reviews with quality larger than 2
+    val studyMetadata: CardStudyMetadata,
 )
---
+++ application/src/main/kotlin/com/memohero/core/domain/card/CardAnswer.kt
@@ -0,0 +1,5 @@
+package com.memohero.core.domain.card
+
+data class CardAnswer(
+    val quality: Int, // The quality of recalling the answer from a scale of 0 to 5
+)
