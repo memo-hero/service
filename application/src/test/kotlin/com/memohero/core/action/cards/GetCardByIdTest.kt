d1faeb78b15d15efdbbe54e573af72cc1b68388b
Victor_Grycuk <victor@grycuk.net>
2023-03-03 00:12:33 -0300

    Fix tests

--- application/src/test/kotlin/com/memohero/core/action/cards/GetCardByIdTest.kt
+++ application/src/test/kotlin/com/memohero/core/action/cards/GetCardByIdTest.kt
@@ -1,6 +1,7 @@
 package com.memohero.core.action.cards
 
 import com.memohero.core.domain.card.CardRepository
+import com.memohero.core.domain.user.UserRepository
 import com.memohero.tools.mothers.getRandomNewCard
 import com.memohero.tools.mothers.getRandomUser
 import org.assertj.core.api.Assertions.assertThat
@@ -10,14 +11,16 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 class GetCardByIdTest {
-    private val mockedRepository: CardRepository = mock()
-    private val getCardById = GetCardById(mockedRepository)
+    private val mockedUserRepository: UserRepository = mock()
+    private val mockedCardRepository: CardRepository = mock()
+    private val getCardById = GetCardById(mockedUserRepository, mockedCardRepository)
     private val card = getRandomNewCard()
     private val user = getRandomUser(id = card.userId)
 
     @BeforeEach
     internal suspend fun setUp() {
-        whenever(mockedRepository.getByUserId(user.id)).thenReturn(listOf(card))
+        whenever(mockedUserRepository.checkUserExists(user.id)).thenReturn(true)
+        whenever(mockedCardRepository.getByUserId(user.id)).thenReturn(listOf(card))
     }
 
     @Test
--- application/src/test/kotlin/com/memohero/core/action/cards/StoreCardTest.kt
+++ application/src/test/kotlin/com/memohero/core/action/cards/StoreCardTest.kt
@@ -1,22 +1,26 @@
 package com.memohero.core.action.cards
 
 import com.memohero.core.domain.card.CardRepository
+import com.memohero.core.domain.user.UserRepository
 import com.memohero.tools.mothers.getRandomNewCard
+import com.memohero.tools.mothers.getRandomUser
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 
 class StoreCardTest {
-    private val mockedRepository: CardRepository = mock()
+    private val mockedUserRepository: UserRepository = mock()
+    private val mockedCardRepository: CardRepository = mock()
+    private val user = getRandomUser()
     private val newCard = getRandomNewCard()
 
     @Test
     suspend fun `should store new cards`() {
-        val storeCard = StoreCard(mockedRepository)
+        val storeCard = StoreCard(mockedUserRepository, mockedCardRepository)
 
-        storeCard(newCard)
+        storeCard(user.id, newCard)
 
-        verify(mockedRepository, times(1)).add(newCard)
+        verify(mockedCardRepository, times(1)).add(newCard)
     }
 }
